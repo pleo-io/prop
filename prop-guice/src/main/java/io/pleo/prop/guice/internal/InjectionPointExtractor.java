8e3f8b3c116c9fddf0e86ce9cfade84c254be18a
Guillaume Simard <guisim@gmail.com>
2017-04-06 17:44:59 -0400

    Added better error messages

--- prop-guice/src/main/java/io/pleo/prop/guice/internal/InjectionPointExtractor.java
+++ prop-guice/src/main/java/io/pleo/prop/guice/internal/InjectionPointExtractor.java
@@ -2,6 +2,7 @@ package io.pleo.prop.guice.internal;
 
 import java.util.function.Predicate;
 
+import com.google.inject.ConfigurationException;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
 import com.google.inject.spi.DefaultBindingTargetVisitor;
@@ -34,7 +35,11 @@ public class InjectionPointExtractor extends DefaultBindingTargetVisitor<Object,
 
   private InjectionPoint getInjectionPointForKey(Key<?> key) {
     if (filter.test(key.getTypeLiteral())) {
-      return InjectionPoint.forConstructorOf(key.getTypeLiteral());
+      try {
+        return InjectionPoint.forConstructorOf(key.getTypeLiteral());
+      } catch (ConfigurationException e) {
+        return null;
+      }
     }
 
     return null;
