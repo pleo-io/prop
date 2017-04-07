d3d3f29069aaf68ce8414fd162bc2b288ce5124d
Guillaume Simard <guisim@gmail.com>
2017-04-07 11:36:09 -0400

    Added better exception handling

--- prop-all/src/test/java/io/pleo/prop/objects/UsesTwiceSameProp.java
+++ prop-all/src/test/java/io/pleo/prop/objects/UsesTwiceSameProp.java
@@ -6,9 +6,22 @@ import javax.inject.Named;
 import io.pleo.prop.core.Prop;
 
 public class UsesTwiceSameProp {
+  private final Prop<String> stringProp1;
+  private final Prop<String> stringProp2;
+
   @Inject
   public UsesTwiceSameProp(@Named("io.pleo.test.prop3") Prop<String> stringProp1,
                            @Named("io.pleo.test.prop3") Prop<String> stringProp2) {
 
+    this.stringProp1 = stringProp1;
+    this.stringProp2 = stringProp2;
+  }
+
+  public Prop<String> getStringProp1() {
+    return stringProp1;
+  }
+
+  public Prop<String> getStringProp2() {
+    return stringProp2;
   }
 }
