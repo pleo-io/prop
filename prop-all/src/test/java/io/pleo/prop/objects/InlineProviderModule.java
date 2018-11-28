18d8ee003087cbc97f17e0f8c708929128cba8d4
Pierre-Alexandre St-Jean <pa@stjean.me>
2018-11-27 21:02:44 -0500

    Add a test case that fails

--- prop-all/src/test/java/io/pleo/prop/objects/InlineProviderModule.java
+++ prop-all/src/test/java/io/pleo/prop/objects/InlineProviderModule.java
@@ -9,13 +9,17 @@ import com.google.inject.Singleton;
 import io.pleo.prop.core.Prop;
 
 public class InlineProviderModule extends AbstractModule {
-  @Override
-  protected void configure() {
-  }
-
   @Provides
   @Singleton
   public InjectedObject hardwell(@Named("io.pleo.test.prop3") Prop<String> w_w) {
     return new InjectedObject();
   }
+
+  @Provides
+  @Singleton
+  @Named("tiesto")
+  public InjectedObject tiesto(@Named("io.pleo.test.prop4") Prop<String> w_w, @Named("io.pleo.test.prop3") Prop<String> u_u) {
+    return new InjectedObject();
+  }
+
 }
--- prop-guice/src/main/java/io/pleo/prop/guice/internal/InjectionPointExtractor.java
+++ prop-guice/src/main/java/io/pleo/prop/guice/internal/InjectionPointExtractor.java
@@ -2,18 +2,28 @@ package io.pleo.prop.guice.internal;
 
 import java.util.function.Predicate;
 
+import com.google.inject.Binding;
 import com.google.inject.ConfigurationException;
 import com.google.inject.Key;
 import com.google.inject.TypeLiteral;
+import com.google.inject.spi.ConstructorBinding;
+import com.google.inject.spi.ConvertedConstantBinding;
 import com.google.inject.spi.DefaultBindingTargetVisitor;
+import com.google.inject.spi.ExposedBinding;
 import com.google.inject.spi.InjectionPoint;
+import com.google.inject.spi.InstanceBinding;
 import com.google.inject.spi.LinkedKeyBinding;
+import com.google.inject.spi.ProviderBinding;
+import com.google.inject.spi.ProviderInstanceBinding;
 import com.google.inject.spi.ProviderKeyBinding;
+import com.google.inject.spi.ProvidesMethodBinding;
+import com.google.inject.spi.ProvidesMethodTargetVisitor;
 import com.google.inject.spi.UntargettedBinding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InjectionPointExtractor extends DefaultBindingTargetVisitor<Object, InjectionPoint> {
+public class InjectionPointExtractor extends DefaultBindingTargetVisitor<Object, InjectionPoint> implements
+                                                                                                 ProvidesMethodTargetVisitor<Object, InjectionPoint>  {
   private static final Logger logger = LoggerFactory.getLogger(InjectionPointExtractor.class);
   private final Predicate<TypeLiteral<?>> filter;
 
@@ -36,6 +46,12 @@ public class InjectionPointExtractor extends DefaultBindingTargetVisitor<Object,
     return getInjectionPointForKey(providerKeyBinding.getProviderKey());
   }
 
+  @Override
+  public InjectionPoint visit(ProvidesMethodBinding<?> providesMethodBinding) {
+    return providesMethodBinding.getDependencies().iterator().next().getInjectionPoint();
+   // return getInjectionPointForKey(providesMethodBinding.getKey());
+  }
+
   private InjectionPoint getInjectionPointForKey(Key<?> key) {
     if (filter.test(key.getTypeLiteral())) {
       try {
@@ -45,7 +61,7 @@ public class InjectionPointExtractor extends DefaultBindingTargetVisitor<Object,
         return null;
       }
     }
-
     return null;
   }
+
 }
